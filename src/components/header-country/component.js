b703e96037a149967bed9becd85141ab775cac61
Migsar Navarro <migsar.navarro@vizzuality.com>
2019-07-25 15:34:15 +0100

    feat(#24): Sub-national ranking formatting edits.

--- src/components/header-country/component.js
+++ src/components/header-country/component.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'vizzuality-components';
-
+import { Field } from 'aqueduct-components';
 
 import IndicatorSelector from 'components/indicator-selector';
 
 const HeaderCountry = ({ onBackClick, data }) => {
   const backClickHandler = () => onBackClick();
+
+  const order = ['Irr', 'Dom', 'Ind', 'Tot'];
+  const headerIndicators = Object.fromEntries(Object.entries(data.indicators)
+    .filter(data => order.includes(data[0]))
+    .sort((a, b) => order.indexOf(a[0]) - order.indexOf(b[0])));
 
   return (
     <header className="c-header">
@@ -15,15 +20,21 @@ const HeaderCountry = ({ onBackClick, data }) => {
         <span className="-uppercase">Country Rankings</span>
       </div>
       <div className="header--content">
-        <div>
+        <div className="header--info">
           <h1>{data.name}</h1>
-          <ul className="header--indicators">
-            {Object.entries(data.indicators).map(indicator => (
-              <li className="header--indicator" key={indicator[0]}>
-                {indicator[1].name}: {indicator[1].score}
-              </li>
-            ))}
-          </ul>
+          <Field
+            name="header-indicators"
+            label="Summary Results"
+            className="-bigger"
+          >
+            <ul className="header--indicators">
+              {Object.entries(headerIndicators).map(indicator => (
+                <li className="header--indicator" key={indicator[0]}>
+                  {indicator[1].name}: {indicator[1].score}
+                </li>
+              ))}
+            </ul>
+          </Field>
         </div>
         <IndicatorSelector className="header--indicator-selector"/>
         <p className="header--disclaimer">This map is for illustrative purposes and does not imply the expression of any opinion on the part of WRI concerning the legal status of any country or territory, or concerning the delimitation of frontiers or boundaries.</p>
