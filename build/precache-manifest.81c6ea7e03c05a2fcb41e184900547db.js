6685d4b9436a18423a693f930415ca319c829d63
Andrés González <andres.gonzalez@vizzuality.com>
2019-08-05 21:12:10 +0200

    Pushes build

---
+++ build/precache-manifest.81c6ea7e03c05a2fcb41e184900547db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f4e7d998352d69a90925faba088cd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c9baa95d974cf1f5661",
+    "url": "./static/css/2.d4adf7a7.chunk.css"
+  },
+  {
+    "revision": "4e1542dcfd6476a7dd0d",
+    "url": "./static/css/main.e5aafd6e.chunk.css"
+  },
+  {
+    "revision": "8c9baa95d974cf1f5661",
+    "url": "./static/js/2.eecb5875.chunk.js"
+  },
+  {
+    "revision": "4e1542dcfd6476a7dd0d",
+    "url": "./static/js/main.53cc8a0d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
