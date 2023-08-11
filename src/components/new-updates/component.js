0187e849ca0712e4a48cc59b9e0b7bdb6b87bc07
Carlos Del Real <carloshdelreal@gmail.com>
2023-08-11 17:49:53 -0500

    feat: update new updates pop up

--- src/components/new-updates/component.js
+++ src/components/new-updates/component.js
@@ -21,9 +21,9 @@ const NewUpdatesModal = () => {
         <h3>New Updates!</h3>
         <button onClick={closeUpdatesModal}>X</button>
       </header>
-      <p>We launched the latest version of Aqueduct, Aqueduct 4.0, on August 16, 2023. This includes changes to the baseline data now using updated inputs to the hydrological model. In addition, we now include future projections data for 2030, 2050 and 2080 based on the latest climate models.</p>
-      <p>For instructions on how to download the data and interpret the CSV colums please see <a target='_blank' href='https://github.com/wri/Aqueduct40/blob/master/data_dictionary_water-risk-atlas.md' rel="noreferrer">this Github link</a>.</p>
-      <p><a target='_blank' href='https://www.eventbrite.com/e/aqueduct-water-risk-atlas-future-projections-a-tool-demonstration-tickets-692074833457?utm_source=aqueduct-tools&utm_medium=banner&utm_campaign=launch&utm_id=launch-webinar' rel="noreferrer"> Register for our Aqueduct 4.0 webinar</a> to learn more</p>
+      <p>The latest version of Aqueduct, Aqueduct 4.0, launched on August 16, 2023 with updated inputs to the hydrological model, providing more accurate baseline data, as well as future projections data for 2030, 2050 and 2080, based on the latest climate models.</p>
+        <p>Find instructions on downloading the data and interpreting the CSV columns at <a target="_blank" href="https://github.com/wri/Aqueduct40/blob/master/data_dictionary_water-risk-atlas.md" rel="noreferrer">this Github link</a>.</p>
+        <p><a target="_blank" href="https://wri.zoom.us/webinar/register/WN_pBPBSJQcQPmaYzhFAC3tGg#/registration%5D" rel="noreferrer"> Register for our Aqueduct 4.0 webinar</a> After registering, you will receive a confirmation email about joining the webinar.  on September 7, 2023 to learn more</p>
       <a className='button-link' target='_blank' href='https://doi.org/10.46830/writn.23.00061' rel="noreferrer">READ FULL DOCUMENTATION</a>
     </div>
   ) : null
